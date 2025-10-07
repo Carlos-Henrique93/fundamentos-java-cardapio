c7dbd334c4ff75f5c340329a3fecf51f893a5684
Carlos-Henrique93 <carlos.henriquesantos@hotmail.com>
2025-10-07 15:36:22 -0300

    Aula Gradle

---
+++ src/main/java/br/com/florinda/cardapio/ItemCardapio.java
@@ -0,0 +1,16 @@
+package br.com.florinda.cardapio;
+
+import java.math.BigDecimal;
+
+public record ItemCardapio(Long id, String nome, String descricao, CategoriaCardapio categoria, BigDecimal preco,
+                           BigDecimal precoComDesconto) {
+
+    public enum CategoriaCardapio {
+        ENTRADA,
+        PRATOS_PRINCIPAIS,
+        BEBIDAS,
+        SOBREMESA;
+    }
+
+
+}
